17bed82912fa9ebba160e2a93fa855aa3d83cad9
JesusAlfaroS <Jalfaro-suarez@csumb.edu>
2025-12-06 18:46:20 -0800

    Inflated PasswordActivity

--- app/src/main/java/com/example/project2/PasswordActivity.java
+++ app/src/main/java/com/example/project2/PasswordActivity.java
@@ -8,17 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.project2.database.RandomlyRepository;
+import com.example.project2.database.entities.User;
+import com.example.project2.databinding.ActivityPasswordBinding;
+
 public class PasswordActivity extends AppCompatActivity {
+
+    private ActivityPasswordBinding binding;
+    private RandomlyRepository repository;
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = RandomlyRepository.getRepository(getApplication());
     }
 }
